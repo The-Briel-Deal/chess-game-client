8b12b40d285f26433552b662cc9f14b9d9b5eb57
Gabriel <gabe@fordltc.net>
2022-08-29 22:50:34 -0400

    Got swapping working

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,44 +1,177 @@
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
+import React, { useEffect } from "react";
 import "./App.css";
 
 export default function Board() {
   const { isLoading, error, data, isFetching } = useQuery(
-    ["repoData"],
+    ["getBoard"],
     (): Promise<Number[][]> =>
       axios.get("http://localhost:8080/getBoard").then((res) => res.data)
   );
+  const [toMove, setToMove] = React.useState<{
+    rowOne: null | number;
+    colOne: null | number;
+    rowTwo: null | number;
+    colTwo: null | number;
+  }>({
+    rowOne: null,
+    colOne: null,
+    rowTwo: null,
+    colTwo: null,
+  });
+  const mutation = useMutation(
+    (positions: {
+      rowOne: Number;
+      colOne: Number;
+      rowTwo: Number;
+      colTwo: Number;
+    }) =>
+      axios
+        .post("http://localhost:8080/swapPiece", positions)
+        .then((res) => res.data),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(["getBoard"]);
+      },
+    }
+  );
+  useEffect(() => {
+    const { rowOne, colOne, rowTwo, colTwo } = toMove;
+    if (
+      rowOne !== null &&
+      colOne !== null &&
+      rowTwo !== null &&
+      colTwo !== null
+    ) {
+      mutation.mutate({ rowOne, colOne, rowTwo, colTwo });
+      setToMove({
+        rowOne: null,
+        colOne: null,
+        rowTwo: null,
+        colTwo: null,
+      });
+    }
+  }, [toMove]);
+  //   const testFunc = () => {
+  //     console.log(toMove);
+  //     if (
+  //       toMove.rowOne !== null &&
+  //       toMove.colOne !== null &&
+  //       toMove.rowTwo !== null &&
+  //       toMove.colTwo !== null
+  //     ) {
+  //       mutation.mutate({
+  //         rowOne: toMove.rowOne,
+  //         colOne: toMove.colOne,
+  //         rowTwo: toMove.rowTwo,
+  //         colTwo: toMove.colTwo,
+  //       });
+  //     }
+  //   };
+  function selectPiece(row: number, col: number) {
+    if (toMove.rowOne === null || toMove.colOne === null) {
+      setToMove({
+        ...toMove,
+        rowOne: row,
+        colOne: col,
+      });
+    } else if (toMove.rowTwo === null || toMove.colTwo === null) {
+      console.log(row, col);
+      setToMove({
+        ...toMove,
+        rowTwo: row,
+        colTwo: col,
+      });
+      //   setTimeout(testFunc, 1000);
+    }
+  }
+  const queryClient = useQueryClient();
+  useMutation;
   return (
     <div style={{ display: "flex" }} className="App">
+      {JSON.stringify(toMove)}
       {data?.map((row, rowIndex) => {
         return (
           <div style={{ flexDirection: "row" }}>
             {row.map((cell, cellIndex) => {
               return (
                 <div
+                  onClick={() => selectPiece(rowIndex, cellIndex)}
                   style={{
                     flexDirection: "column",
                     height: 50,
                     width: 50,
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    fontSize: "3em",
                     backgroundColor: `${
-                      (rowIndex + cellIndex) % 2 != 0 ? "black" : "white"
+                      rowIndex === toMove.rowOne && cellIndex === toMove.colOne
+                        ? "#FFCCCB"
+                        : (rowIndex + cellIndex) % 2 != 0
+                        ? "grey"
+                        : "white"
                     }`,
-                    color: `${
-                      (rowIndex + cellIndex) % 2 != 0 ? "white" : "black"
-                    }`,
+                    color: "black",
                   }}
                 >
-                  {cell.toString()}
+                  <span>
+                    {(() => {
+                      switch (cell) {
+                        case 0:
+                          return "";
+                        case 1:
+                          return "♙";
+                        case 2:
+                          return "♖";
+                        case 3:
+                          return "♘";
+                        case 4:
+                          return "♗";
+                        case 5:
+                          return "♕";
+                        case 6:
+                          return "♔";
+                        case 7:
+                          return "♟";
+                        case 8:
+                          return "♜";
+                        case 9:
+                          return "♞";
+                        case 10:
+                          return "♝";
+                        case 11:
+                          return "♛";
+                        case 12:
+                          return "♚";
+                        default:
+                          return "";
+                      }
+                    })()}
+                  </span>
                 </div>
               );
             })}
           </div>
         );
       })}
+      <div style={{ flexDirection: "row" }}>
+        <div style={{ flexDirection: "column" }}>
+          <button
+            onClick={() => {
+              mutation.mutate({
+                rowOne: 0,
+                colOne: 3,
+                rowTwo: 0,
+                colTwo: 1,
+              });
+            }}
+          >
+            test
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
